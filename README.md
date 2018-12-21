92b41828fb84b7ad44bd3ef2f453bbbdbe501059
Benjamin Lannon <benjamin@lannonbr.com>
2018-12-21 12:52:15 -0500

    Updating NPX card

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-It's me, Tierney!
+A npx card based on [bnb/bitandbang](https://github.com/bnb/bitandbang)
 
 # Usage
-Via npx:
-```
-npx bitandbang
-```
 
-Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
+Via npx:
+
+```
+npx lannonbr
+```
--- bin/card.js
+++ bin/card.js
@@ -2,45 +2,56 @@
 // 👆 Used to tell Node.js that this is a CLI tool
 
 // Pull in our modules
-const chalk = require('chalk')
-const boxen = require('boxen')
+const chalk = require("chalk");
+const boxen = require("boxen");
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
-  borderStyle: 'round'
-}
+  borderStyle: "round"
+};
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:')
-}
+  name: chalk.white("Benjamin Lannon /"),
+  handle: chalk.green("lannonbr"),
+  work: chalk.white("Web Developer"),
+  twitter: chalk.green("https://twitter.com/lannonbr"),
+  github: chalk.green("https://github.com/lannonbr"),
+  web: chalk.green("https://lannonbr.com"),
+  npx: chalk.white("npx lannonbr"),
+  labelWork: chalk.white.bold("      Work:"),
+  labelTwitter: chalk.white.bold("   Twitter:"),
+  labelGitHub: chalk.white.bold("    GitHub:"),
+  labelLinkedIn: chalk.white.bold("  LinkedIn:"),
+  labelWeb: chalk.white.bold("       Web:"),
+  labelCard: chalk.white.bold("      Card:")
+};
 
 // Actual strings we're going to output
-const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const newline = "\n";
+const heading = `${data.name} ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output =
+  heading +
+  newline +
+  newline +
+  working +
+  newline +
+  twittering +
+  newline +
+  githubing +
+  newline +
+  webing +
+  newline +
+  newline +
+  carding;
 
-console.log(chalk.green(boxen(output, options)))
+console.log(chalk.green(boxen(output, options)));
